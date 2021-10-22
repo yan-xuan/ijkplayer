766bf49f71204ddcf2a62d480f5e9174d4d08dbb
yanxuan <heran@xiaohongshu.com>
2021-10-22 14:34:38 +0800

    完善脚本

--- android/cp-release.sh
+++ android/cp-release.sh
@@ -1,22 +1,27 @@
 #! /usr/bin/env bash
-pwd
-
-echo
-
-find . | grep -Ei "\.aar" 2>/dev/null
-
+echo start 
+echo remove old "release" dir
 rm -rf release 
 
+echo find aar file
+find . | grep -Ei "\.aar" 2>/dev/null
+echo
+
+#_pwd=`pwd`
+#echo ${_pwd}
+echo create new release dir: `pwd`/release
 mkdir release 
 
+echo copy armv7a and arm64 related file
 cp ./ijkplayer/ijkplayer-arm64/build/outputs/aar/ijkplayer-arm64-release.aar ./release  
 cp ./ijkplayer/ijkplayer-armv7a/build/outputs/aar/ijkplayer-armv7a-release.aar ./release
 cp ./ijkplayer/ijkplayer-java/build/outputs/aar/ijkplayer-java-release.aar ./release  
 
-echo
+echo 'release' dir file list:
 
 find ./release | grep -Ei "\.aar" 2>/dev/null
 
 echo
 
 exit
+
